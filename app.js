0d656f3024bacf969bdcb4f437486ca956532bb3
Muhammad Hammad <itshammadofficial@gmail.com>
2023-03-29 22:06:21 +0500

    minor bug fixes

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 let topRow = document.querySelector(".top-row");
 let bottomRow = document.querySelector(".bottom-row");
 let logo = document.querySelector(".logo-img");
-// let sidebar = document.querySelector(".sidebar");
-// let hamburger = document.getElementById("hamburger");
-// let closeIcon = document.getElementById("close");
-// let overlay = document.getElementById("overlay");
-// let showNavbar = document.getElementById("show-navbar");
+let toggle_bar = document.querySelector(".nav-header");
+let sidebar = document.querySelector(".sidebar");
 
 window.addEventListener("scroll", () => {
   // console.log(window.scrollY);
@@ -16,29 +13,8 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// hamburger.addEventListener("click", () => {
-//   overlay.classList.add("overlay");
-//   showNavbar.classList.add("show-navbar");
-//   sidebar.style.display = "block";
-//   sidebar.style.transform = "tranlateX(-800px)"
-//   console.log("got clicked");
-// });
-
-// closeIcon.addEventListener("click", () => {
-//   overlay.classList.remove("overlay");
-//   showNavbar.classList.remove("show-navbar");
-//   sidebar.style.display = "none";
-
-// });
-
-
-
-
-
-let toggle_bar = document.querySelector(".nav-header");
-let sidebar = document.querySelector(".sidebar");
-
 toggle_bar.addEventListener("click", function(){
+  console.log(toggle_bar.firstElementChild);
     if(toggle_bar.firstElementChild.classList.contains("fa-bars"))
     {
         toggle_bar.firstElementChild.classList.replace("fa-bars","fa-times");
