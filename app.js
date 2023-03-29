b4cdf0f7a0f20326c6cb8560d99d0ce8b5525164
Muhammad Hammad <itshammadofficial@gmail.com>
2023-03-29 20:29:02 +0500

    Remove menu items on small devices and add a hamburger and sidebar with javascript

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let topRow = document.querySelector(".top-row");
 let bottomRow = document.querySelector(".bottom-row");
 let logo = document.querySelector(".logo-img");
+// let sidebar = document.querySelector(".sidebar");
+// let hamburger = document.getElementById("hamburger");
+// let closeIcon = document.getElementById("close");
+// let overlay = document.getElementById("overlay");
+// let showNavbar = document.getElementById("show-navbar");
 
 window.addEventListener("scroll", () => {
   // console.log(window.scrollY);
@@ -10,3 +15,37 @@ window.addEventListener("scroll", () => {
     bottomRow.classList.remove("navScroll");
   }
 });
+
+// hamburger.addEventListener("click", () => {
+//   overlay.classList.add("overlay");
+//   showNavbar.classList.add("show-navbar");
+//   sidebar.style.display = "block";
+//   sidebar.style.transform = "tranlateX(-800px)"
+//   console.log("got clicked");
+// });
+
+// closeIcon.addEventListener("click", () => {
+//   overlay.classList.remove("overlay");
+//   showNavbar.classList.remove("show-navbar");
+//   sidebar.style.display = "none";
+
+// });
+
+
+
+
+
+let toggle_bar = document.querySelector(".nav-header");
+let sidebar = document.querySelector(".sidebar");
+
+toggle_bar.addEventListener("click", function(){
+    if(toggle_bar.firstElementChild.classList.contains("fa-bars"))
+    {
+        toggle_bar.firstElementChild.classList.replace("fa-bars","fa-times");
+    }
+    else
+    {
+        toggle_bar.firstElementChild.classList.replace("fa-times","fa-bars");
+    }
+   sidebar.classList.toggle("sidebaractive")
+  })
